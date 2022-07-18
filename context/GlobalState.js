ec3820c0e41b14f1a80f4d643264bf1152cecd48
Vs-karma <81770024+Vs-karma@users.noreply.github.com>
2022-07-18 21:29:46 +0530

    changes

--- context/GlobalState.js
+++ context/GlobalState.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react'
 import Context from './Context'
+import { WishlistPushUrl } from '../urls';
 
 export default function GlobalState({ children }) {
     const [user, setuser] = useState('UdayKiran');
 
+
+    // const authtoken = localStorage.getItem('token');
+
     const [wlist, setWlist] = useState({});
     const pushwList = async () => {
-        const res = await fetch('url', {
+        const res = await fetch(WishlistPushUrl, {
             method: 'POST',
             body: JSON.stringify({ wlist }),
             headers: {
                 'CONTENT-TYPE': 'application/json',
+                'auth-token': authtoken
             }
         });
         const data = await res.json();
