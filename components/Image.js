1f00cf92afd09fc61a825a3040bc1ad7e863530a
Vs-karma <81770024+Vs-karma@users.noreply.github.com>
2022-07-31 17:32:46 +0530

    single product

--- components/Image.js
+++ components/Image.js
@@ -4,10 +4,10 @@ import Image from 'next/image'
 const Images = () => {
   return (
     <div className="prodimg">
-    <Image src="/metal.png" alt="steel" className='prodimg'
-width={50}
-height={50}/> 
-</div>
+      <Image src="/metal.png" alt="steel" className='prodimg'
+        width={50}
+        height={50} />
+    </div>
   )
 }
 
