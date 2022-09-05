06ed2e18345d719394253b3876b58f738f07c682
Udaykiran <officialudaykiran@gmail.com>
2022-09-05 19:58:46 +0530

    .

--- pages/_app.js
+++ pages/_app.js
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }) {
         <Navbar />
         <br />
         <Head>
+          <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous" />
           <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css" />
         </Head>
