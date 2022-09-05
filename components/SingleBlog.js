6e00b334d1342eb65e913bf59236249b3275214f
Udaykiran <officialudaykiran@gmail.com>
2022-09-05 20:47:11 +0530

    added priority

--- components/SingleBlog.js
+++ components/SingleBlog.js
@@ -3,39 +3,39 @@ import Link from "next/link"
 import Image from 'next/image'
 import { backendurl } from '../urls'
 import { useRouter } from 'next/router';
-export default function SingleBlog({blog}) {
-        let date = new Date(blog.createdAt); 
-        let displaydate = date.getDay().toString()+ '-' + (date.getMonth()+1).toString()+'-' + date.getFullYear(); 
-        
-        let link = '/blogs/'+blog._id; 
+export default function SingleBlog({ blog }) {
+        let date = new Date(blog.createdAt);
+        let displaydate = date.getDay().toString() + '-' + (date.getMonth() + 1).toString() + '-' + date.getFullYear();
 
-        const router = useRouter(); 
+        let link = '/blogs/' + blog._id;
+
+        const router = useRouter();
 
         return (
-        <>
-                <div className="singleBlog" onClick={()=>router.push(link)}>
-                        <div className="blogTop">
-                                <Image src={backendurl+blog.img} alt="Blog_Image" layout="fill" className="blogImage" />
-                        </div>
-                        <div className="blogBottom">
-                                <div className="info">
-                                        <p className="blog_small">
-                                                <i className="icon uil-clock uil"></i>
+                <>
+                        <div className="singleBlog" onClick={() => router.push(link)}>
+                                <div className="blogTop">
+                                        <Image src={backendurl + blog.img} priority alt="Blog_Image" layout="fill" className="blogImage" />
+                                </div>
+                                <div className="blogBottom">
+                                        <div className="info">
+                                                <p className="blog_small">
+                                                        <i className="icon uil-clock uil"></i>
                                                         {
-                                                                displaydate        
+                                                                displaydate
                                                         }
-                                        </p>
-                                        <p className="blog_small">
-                                                <i className="icon  uil-pricetag-alt uil"></i>
-                                                {blog.category[0]}  
-                                        </p>
-                                </div>
-                                <div className="details">
-                                        <h5 className="blog_title">{blog.title}</h5>
-                                        <p className="blog_desc">{blog.description.slice(0,90)+"..."}</p>
+                                                </p>
+                                                <p className="blog_small">
+                                                        <i className="icon  uil-pricetag-alt uil"></i>
+                                                        {blog.category[0]}
+                                                </p>
+                                        </div>
+                                        <div className="details">
+                                                <h5 className="blog_title">{blog.title}</h5>
+                                                <p className="blog_desc">{blog.description.slice(0, 90) + "..."}</p>
+                                        </div>
                                 </div>
                         </div>
-                </div>
-        </>
-  )
+                </>
+        )
 }
